f367b44031a9516e01371f5aebe0ca1ff6d3217d
yusuf bestas <bestasyusuf8@gmail.com>
2022-02-27 12:07:20 +0300

    created project

---
+++ src/test/java/com/stroe/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stroe.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
